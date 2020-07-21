1a9e3cdc3a336cb3640cad26a086acbeca26c564
IvanSabV <60038980+IvanSabV@users.noreply.github.com>
2020-07-21 22:15:54 +0400

    upload files

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserRepository userRepository = new UserRepository();
+
+            Console.WriteLine(userRepository.GetUserById());
+            Console.WriteLine(userRepository.GetUserByName());
+            Console.WriteLine(userRepository.SearchUserByName());
+            Console.WriteLine(userRepository.AddUser());
+            Console.WriteLine(userRepository.UpdateUser());
+            Console.WriteLine(userRepository.UpdateUserState());
+
+
+            MessageRepository messageRepository = new MessageRepository();
+
+            Console.WriteLine(messageRepository.GetUserMessages());
+            Console.WriteLine(messageRepository.SearchUserMessages());
+            Console.WriteLine(messageRepository.AddMessage());
+
+
+            ContactRepository contactRepository = new ContactRepository();
+
+            Console.WriteLine(contactRepository.GetUserContact());
+            Console.WriteLine(contactRepository.GetUserContacts());
+            Console.WriteLine(contactRepository.SearchUserContacts());
+            Console.WriteLine(contactRepository.AddContact());
+            Console.WriteLine(contactRepository.UpdateContact());
+            Console.WriteLine(contactRepository.DeleteContact());
+
+        }
+    }
+
+
+}
---
+++ ConsoleApp1/UserRepository.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Runtime.Remoting.Messaging;
+
+namespace ConsoleApp1
+{
+
+	public class UserRepository
+	{
+		public string GetUserById()
+		{
+			string connStr = "server=DESKTOP-VVD3IHK\\SQLEXPRESS;user=admin;database=UserContactMessage;password=123;";
+
+			SqlConnection conn = new SqlConnection(connStr);
+
+			conn.Open();
+
+			string sql = "SELECT name FROM UserDataModel WHERE id = 1";
+
+			SqlCommand command = new SqlCommand(sql, conn);
+
+			string name = command.ExecuteScalar().ToString();
+
+			Console.WriteLine(name);
+
+			return null;
+
+		}
+				
+		public string GetUserByName()
+		{
+			string connStr = "server=DESKTOP-VVD3IHK\\SQLEXPRESS;user=admin;database=UserContactMessage;password=123;";
+
+			SqlConnection conn = new SqlConnection(connStr);
+
+			conn.Open();
+
+			string sql = "SELECT id,name FROM UserDataModel WHERE name = 'admin'";
+
+			SqlCommand command = new SqlCommand(sql, conn);
+
+			SqlDataReader reader = command.ExecuteReader();
+
+			while(reader.Read())
+            {
+				Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString());
+			}
+			reader.Close();
+
+			return null;
+			
+		}
+		public string SearchUserByName()
+		{
+			string connStr = "server=DESKTOP-VVD3IHK\\SQLEXPRESS;user=admin;database=UserContactMessage;password=123;";
+
+			SqlConnection conn = new SqlConnection(connStr);
+
+			conn.Open();
+
+			string sql = "SELECT * FROM UserDataModel WHERE name like 'ea%'";
+
+			SqlCommand command = new SqlCommand(sql, conn);
+
+			SqlDataReader reader = command.ExecuteReader();
+
+			while (reader.Read())
+			{
+				Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString() + " " + reader[3].ToString());
+			}
+			reader.Close();
+
+			return null;
+
+		}
+		
+		public string AddUser()
+		{
+			string connStr = "server=DESKTOP-VVD3IHK\\SQLEXPRESS;user=admin;database=UserContactMessage;password=123;";
+
+			SqlConnection conn = new SqlConnection(connStr);
+
+			conn.Open();
+
+			string sql = "SELECT max(id)+1 FROM UserDatamodel INSERT INTO UserDatamodel VALUES(alex, 'pass', 1)";
+
+			SqlCommand command = new SqlCommand(sql, conn);
+
+
+			return null;
+		}
+
+		public string UpdateUser()
+		{
+			string connStr = "server=DESKTOP-VVD3IHK\\SQLEXPRESS;user=admin;database=UserContactMessage;password=123;";
+
+			SqlConnection conn = new SqlConnection(connStr);
+
+			conn.Open();
+
+			string sql = "UPDATE userDataModel SET name = 'alex' WHERE id = 7";
+
+			SqlCommand command = new SqlCommand(sql, conn);
+
+
+
+			return null;
+
+		}
+		
+		public string UpdateUserState()
+		{
+			string connStr = "server=DESKTOP-VVD3IHK\\SQLEXPRESS;user=admin;database=UserContactMessage;password=123;";
+
+			SqlConnection conn = new SqlConnection(connStr);
+
+			conn.Open();
+
+			string sql = "UPDATE userDataModel SET state = 0 WHERE id = 4 SELECT * FROM UserDataModel WHERE id = 4";
+
+			SqlCommand command = new SqlCommand(sql, conn);
+
+			SqlDataReader reader = command.ExecuteReader();
+
+			while (reader.Read())
+			{
+				Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString() + " " + reader[3].ToString());
+			}
+			reader.Close();
+
+			return null;
+
+		}
+
+	
+	}
+}
